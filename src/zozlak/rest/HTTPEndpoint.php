0d2951850d4be2d1b328a3b357445c1dd5e8e809
zozlak <zozlak@zozlak.org>
2016-07-09 18:24:58 +0200

    - input variables are now shared among all instances (static) as the script input is static as well - HTTPEndpoint holds reference to its HTTPController and no longer takes config object directly

--- src/zozlak/rest/HTTPEndpoint.php
+++ src/zozlak/rest/HTTPEndpoint.php
@@ -32,99 +32,122 @@ namespace zozlak\rest;
  * @author zozlak
  */
 class HTTPEndpoint {
-	private $args;
-	/**
-	 *
-	 * @var type \util\Config
-	 */
-	protected $config;
-	
-	public function __construct(\stdClass $path, $config) {
-		foreach($path as $key => $value){
-			$this->$key = $value;
-		}
-		$this->config = $config;
-	}
-	
-	public function get(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function head(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function post(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function put(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function delete(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function trace(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function options(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function connect(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function patch(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function getCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function postCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function putCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function deleteCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function traceCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function optionsCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function connectCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	public function patchCollection(FormatterInterface $f){
-		throw new \BadMethodCallException('Method not implemented');
-	}
-	
-	public function filterInput($name){
-		$method = filter_input(\INPUT_SERVER, 'REQUEST_METHOD');
-		if($method === 'GET'){
-			return filter_input(\INPUT_GET, $name);
-		}else if($method === 'POST'){
-			return filter_input(\INPUT_POST, $name);
-		}else{
-			if($this->args === null){
-				parse_str(file_get_contents("php://input"), $this->args);
-			}
-			if(!array_key_exists($name, $this->args)){
-				return null;
-			}
-			return $this->args[$name];
-		}
-	}
-	
-	public function toUnderscore($name){
-		$parts = preg_split('/[A-Z]/', $name);
-		$n = mb_strlen($parts[0]);
-		$res = $parts[0];
-		foreach($parts as $k => $i){
-			if($k !== 0){
-				$res .= '_' . strtolower(mb_substr($name, $n, 1)). $i;
-				$n += mb_strlen($i) + 1;
-			}
-		}
-		return $res;
-	}
+
+    static private $args;
+
+    /**
+     *
+     * @var type \zozlak\rest\HTTPContoller
+     */
+    protected $controller;
+
+    public function __construct(\stdClass $path, HTTPContoller $controller) {
+        $this->controller = $controller;
+        foreach ($path as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    public function getConfig(){
+        return $this->controller->config;
+    }
+    
+    public function get(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function head(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function post(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function put(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function delete(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function trace(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function options(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function connect(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function patch(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function getCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function postCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function putCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function deleteCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function traceCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function optionsCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function connectCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function patchCollection(FormatterInterface $f) {
+        throw new \BadMethodCallException('Method not implemented');
+    }
+
+    public function filterInput($name) {
+        $method = filter_input(\INPUT_SERVER, 'REQUEST_METHOD');
+        if ($method === 'GET') {
+            return filter_input(\INPUT_GET, $name);
+        } else if ($method === 'POST') {
+            return filter_input(\INPUT_POST, $name);
+        } else {
+            if (self::$args === null) {
+                parse_str(file_get_contents("php://input"), self::$args);
+            }
+            if (!array_key_exists($name, self::$args)) {
+                return null;
+            }
+            return self::$args[$name];
+        }
+    }
+
+    public function toUnderscore($name) {
+        $parts = preg_split('/[A-Z]/', $name);
+        $n = mb_strlen($parts[0]);
+        $res = $parts[0];
+        foreach ($parts as $k => $i) {
+            if ($k !== 0) {
+                $res .= '_' . strtolower(mb_substr($name, $n, 1)) . $i;
+                $n += mb_strlen($i) + 1;
+            }
+        }
+        return $res;
+    }
+
 }
